556a384c6b5db190eb9086da412439d6731fdae2
BIGTREETC\nguyenxuan.tung <tung.nguyenxuan@vietis.com.vn>
2021-09-01 10:50:32 +0700

    commit 1 add coomit 2 comit 3 commit 5

--- src/component/home.js
+++ src/component/home.js
@@ -4,6 +4,10 @@ function Home() {
     return (
       <div className="home">
         HOME
+        <p>Thêm commit</p>
+        <p>Thêm commit222222222222</p>
+        <p>Thêm commit333333333333333</p>
+        <p>Thêm commit 55555555555555</p>
       </div>
     );
   }
